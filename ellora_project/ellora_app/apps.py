215fe6b1ade84d55238f7be97ea28e40a4a924a9
Harsh <vharsh2003@gmail.com>
2025-01-17 14:08:01 +0530

    new mandatory files

--- ellora_project/ellora_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElloraAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ellora_app'
--- ellora_project/ellora_app/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest
-def home(request):
-    return render(request,'ellora_app/index.html')
